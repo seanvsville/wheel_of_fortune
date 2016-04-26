a204fc08bc1b4dc2186868f1a5424eec1bbd9b6f
seanvsville <seanvanho@gmail.com>
2016-04-25 22:45:09 -0700

    progress so far

--- wheel_of_fortune.rb
+++ wheel_of_fortune.rb
@@ -1,19 +1,44 @@
 class WheelOfFortune
-  def initialize
-    p "Happy coding!"
+  attr_reader :theme, :guesses
+
+  def initialize(guess)
+    @theme = guess[:theme]
+    @phrase = guess[:phrase]
+    @guesses = []
+    @remaining_characters = unique_chars
   end
 
   def to_s
-    nil
+    return @phrase if game_over?
+    @phrase.gsub(unguessed, "_")
   end
 
   def can_i_have?(input)
-    nil
+    @guesses << input.downcase
+    @phrase.downcase.include? input.downcase
   end
 
   def game_over?
-    nil
+    @remaining_characters.empty?
   end
+
+  def unguessed
+    Regexp.new(@remaining_characters.join('|'), Regexp::IGNORECASE)
+  end
+
+  def unique_chars
+    @phrase.downcase.delete("^a-z").split("").uniq
+  end
+
+  def add_guess(gess)
+    @guesses << gess unless gess.empty?
+  end
+
+  def good_guess?(char)
+    !!(add_gess(char) && @remaining_characters.delete(char))
+  end
+
+#end of Class WheelOfFortune
 end
 
 
